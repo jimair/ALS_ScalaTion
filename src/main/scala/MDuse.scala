a61b8cc0229902aa642ec885a88603813c25cb89
Pandaxubo <55469347+Pandaxubo@users.noreply.github.com>
2021-02-10 20:51:51 -0500

    version2

--- src/main/scala/MDuse.scala
+++ src/main/scala/MDuse.scala
@@ -1,35 +1,22 @@
 import scalation.linalgebra.MatrixD
 import scalation.linalgebra.Eigenvalue
 import scalation.linalgebra.Eigenvector
+import scalation.linalgebra._
+import scalation.linalgebra.Fac_LU
 import scala.util.Random
 
-class MDuse(a: MatrixD){
+import MatrixD.eye
 
+
+
+class MDuse(a: MatrixD){
+  val MatrixD.DEBUG = false
   //Initialize parameters
   var r_lambda = 40 //normalization parameter
   var nf = 200  //dimension of latent vector of each user and item
   val alpha = 40  //confidence level
   val ni = a.dim2 //number of items 
   val nu = a.dim1 //number of users 
-
-  //Test function
-  def testf = {
-    // val e = (new Eigenvalue (a)).getE ()
-    // val v = (new Eigenvector (a, e)).getV
-    // println(s"e = "+ e + "v = "+v)
-    // println(s"d1 = "+a.dim1+"d2 = "+a.dim2)
-
-    // val nu = a.dim1
-    // val ni = a.dim2
-    // val X = createRM(nu, nf)
-    // val Y = createRM(ni, nf)
-    // println("X:" + X.toString())
-    // println( "dim1 = "+ X.dim1 + "," + "dim2 = "+ X.dim2  )
-    //  println("Y:" + Y.toString())
-    // println( "dim1 = "+ Y.dim1 + "," + "dim2 = "+ Y.dim2  )
-
-    
-  }
 
 
   def createRM(row_number : Int, column_number : Int) : MatrixD = {
@@ -53,15 +40,6 @@ class MDuse(a: MatrixD){
 			}
 		}
     S
-  }
-
-  //Initialize user and item matrix based on input matrix
-  def data_preprocess : (MatrixD, MatrixD) = { 
-    val X = createRM(nu, nf)
-    val Y = createRM(ni, nf)
-    // println("X:" + X.toString())
-    // println( "dim1 = "+ X.dim1 + "," + "dim2 = "+ X.dim2  )
-    (X,Y)
   }
 
   // Initialize Binary Rating Matrix P
@@ -97,18 +75,60 @@ class MDuse(a: MatrixD){
     (predict_error.sum, confidence_error, regularization, total_loss)
   }
 
+  def diagMR(mat : MatrixD, tarRow: Int) : MatrixD = {
+    val tempMat = mat.selectRows(Array(tarRow))
+    val tarMat = new MatrixD(tempMat.dim2, tempMat.dim2)
+    for(i <- 0 to tempMat.dim2 -1){
+        tarMat(i,i) = tempMat(0,i)
+    }
+    tarMat
+  }
+ 
+  
+  def diagMC(mat : MatrixD, tarCol: Int) : MatrixD = {
+    val tempMat = mat.selectCols(Array(tarCol))
+    val tarMat = new MatrixD(tempMat.dim1, tempMat.dim1)
+    for(i <- 0 to tempMat.dim1 -1){
+        tarMat(i,i) = tempMat(i,0)
+    }
+    tarMat
+  }
+
+  def setRow(mat : MatrixD, tarRow: VectoD, index: Int) : MatrixD = {
+    
+    for(j <- 0 to mat.dim2 -1){
+        mat(index,j) = tarRow(j)
+    }
+    mat
+  }
+  
+  def makeVec(mat : MatrixD) : VectoD = {
+    val vec = new VectorD(mat.dim1)
+    
+    for(j <- 0 to mat.dim1 -1){
+        vec(j) = mat(j,0)
+    }
+    vec
+  }
+  
 
 
-  // def optimize_user(X: MatrixD, Y: MatrixD, C: MatrixD, P: MatrixD, nu:Int, nf:Int, r_lambda:Int)={
-  //   yT = Y.t
-
-  //   for (i <- 0 to nu-1 ) {
-  //     Cu = C[i].diag
-  //     yT_Cu_y = outer(yT, Cu) * Y
-  //     lI = eye(nf) * r_lambda
-  //     yT_Cu_pu = outer(yT, Cu) dot P[i]
-  //     X[i] = n
-  //   }
+  def optimize_user(X: MatrixD, Y: MatrixD, C: MatrixD, P: MatrixD, nu:Int, nf:Int, r_lambda:Int)={
+    val yT = Y.t
+    
+    for (i <- 0 to nu-1 ) {
+      val Cu = diagMR(C, i)
+      
+      val yT_Cu_y = yT * Cu * Y
+      val lI = eye(nf) * r_lambda
+      val yT_Cu_pu = makeVec((yT * Cu) * ((P.selectRows(Array(i))).t))
+    
+        //X(i,j) = MatrixD.bsolve(yT_Cu_y + lI, yT_Cu_pu)
+      //setRow(X, (yT_Cu_y + lI).solve(yT_Cu_pu), i)
+      println((yT_Cu_y + lI).solve(yT_Cu_pu))
+      //setRow(X, , i)
+      
+    }
 
   //   for u in range(nu):
   //       Cu = np.diag(C[u])
@@ -116,7 +136,19 @@ class MDuse(a: MatrixD){
   //       lI = np.dot(r_lambda, np.identity(nf))
   //       yT_Cu_pu = np.matmul(np.matmul(yT, Cu), P[u])
   //       X[u] = np.linalg.solve(yT_Cu_y + lI, yT_Cu_pu)
-  // }
+  }
+
+  def optimize_item(X: MatrixD, Y: MatrixD, C: MatrixD, P: MatrixD, nu:Int, nf:Int, r_lambda:Int)={
+    val xT = X.t
+
+    for (i <- 0 to Y.dim2-1 ) {
+      val Ci = diagMC(C, i)
+      val xT_Ci_x = xT * Ci * X
+      val lI = eye(nf) * r_lambda
+      val xT_Ci_pi = makeVec((xT * Ci) * (P.selectCols(Array(i))))
+      Y.setCol(i, (xT_Ci_x + lI).solve(xT_Ci_pi))
+    }
+  }
     
 
 }
@@ -141,7 +173,7 @@ object MDTest extends App{
   val Y = target.createRM(target.ni, target.nf)
   // println("X = "+ X.toString)
   // println("Y = "+ Y.toString)
-  val predict = X * (Y.t)
+  
 
   val p = target.ConP(target.nu,target.ni)
   val c = target.ConfC(r) 
@@ -160,7 +192,35 @@ object MDTest extends App{
 
   // val total_loss = confidence_error + regularization
   // println("4-"+total_loss)
-  val (predict_error, confidence_error, regularization, total_loss) = target.Lossf(c, p, predict, X, Y, target.r_lambda)
+  var predict_errors = ""
+  var confidence_errors = ""
+  var regularization_list = ""
+  var total_losses = ""
+  
+  for (i <- 0 to 15) {
+    if (i != 0){
+        target.optimize_user(X, Y, c, p, target.nu, target.nf, target.r_lambda)
+        //target.optimize_item(X, Y, c, p, target.ni, target.nf, target.r_lambda)
+    }
+    var predict = X * (Y.t)
+    var (predict_error, confidence_error, regularization, total_loss) = target.Lossf(c, p, predict, X, Y, target.r_lambda)
 
+    // predict_errors += predict_error
+    // confidence_errors += confidence_error
+    // regularization_list += regularization
+    // total_losses += total_loss
+
+    println("----------------step "+i+"----------------")
+    // println("predict error: " + predict_error)
+    // println("confidence error: " + confidence_error)
+    // println("regularization: " + regularization)
+    // println("total loss: " + total_loss)
+  }
+  
+    
+  var predict = X * (Y.t)
+
+  println("final predict")
+  //println(predict)
 
 }
